98c9c7bb62f2b7b9859a1fa4d803a3851748c90c
Musk-max <76408168+Musk-max@users.noreply.github.com>
2024-01-04 02:33:56 +0100

    correction des syntaxes sql
    correction des syntaxes pour création des tables dans la base de données

--- tables_club_sportif.sql
+++ tables_club_sportif.sql
@@ -1,35 +1,34 @@
-
-
 CREATE TABLE licencies (
-licencie_id INT AUTO_INCREMENT PRIMARY KEY,
-nom VARCHAR(30),
-prenom VARCHAR(30),
-contact_id INT
-FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
+    licencie_id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(30),
+    prenom VARCHAR(30),
+    contact_id INT,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
 );
 
 CREATE TABLE categories (
-categorie_id INT AUTO_INCREMENT PRIMARY KEY,
-code_raccourci VARCHAR(30),
-nom VARCHAR(30) NOT NULL
+    categorie_id INT AUTO_INCREMENT PRIMARY KEY,
+    code_raccourci VARCHAR(30),
+    nom VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE contacts (
-contact_id INT AUTO_INCREMENT PRIMARY KEY,
-nom VARCHAR(30),
-prenom VARCHAR(30),
-email VARCHAR (30),
-numero VARCHAR (10)
+    contact_id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(30),
+    prenom VARCHAR(30),
+    email VARCHAR(30),
+    numero VARCHAR(10)
 );
 
 CREATE TABLE educateurs (
-educateur_id INT PRIMARY KEY ,
-email VARCHAR(30),
-mdp VARCHAR(30),
-FOREIGN KEY educateur_id  REFERENCES licencies(licencie_id)
+    educateur_id INT PRIMARY KEY,
+    email VARCHAR(30),
+    mdp VARCHAR(30),
+    FOREIGN KEY (educateur_id) REFERENCES licencies(licencie_id)
 );
 
 CREATE TABLE admins (
-admin_id INT PRIMARY KEY ,
-FOREIGN KEY admin_id  REFERENCES educateurs(educateur_id)
+    admin_id INT PRIMARY KEY,
+    FOREIGN KEY (admin_id) REFERENCES educateurs(educateur_id)
 );
+
