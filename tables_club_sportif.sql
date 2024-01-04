6c77c0a9d6a08884aede97e28eaccfa386a843cc
Musk-max <76408168+Musk-max@users.noreply.github.com>
2024-01-04 01:55:46 +0100

    creation des tables de club sportif
    creation des tables de notre projet

---
+++ tables_club_sportif.sql
@@ -0,0 +1,35 @@
+--creation des tables de notre base de données
+
+CREATE TABLE licencies (
+licencie_id INT AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR(30),
+prenom VARCHAR(30),
+contact_id INT
+FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
+);
+
+CREATE TABLE categories (
+categorie_id INT AUTO_INCREMENT PRIMARY KEY,
+code_raccourci VARCHAR(30),
+nom VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE contacts (
+contact_id INT AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR(30),
+prenom VARCHAR(30),
+email VARCHAR (30),
+numero VARCHAR (10)
+);
+
+CREATE TABLE educateurs (
+educateur_id INT PRIMARY KEY ,
+email VARCHAR(30),
+mdp VARCHAR(30),
+FOREIGN KEY educateur_id  REFERENCES licencies(licencie_id)
+);
+
+CREATE TABLE admins (
+admin_id INT PRIMARY KEY ,
+FOREIGN KEY admin_id  REFERENCES educateurs(educateur_id)
+);
