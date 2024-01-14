f788faa1203ca909a912b0cd537a97755e9ec892
LisaNgoufack <zebazelisa@gmail.com>
2024-01-14 22:11:57 +0100

    navbar

---
+++ partials/_header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Accueil</font></font></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Basculer la navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="../../views/views_categories/home.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Catégorie</font></font></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../../views/views_contacts/home.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Contact</font></font></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../../views/views_educateurs/home.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Educateur</font></font></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../../views/views_licencies/home.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Licencié</font></font></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ partials/_header1.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Sport_academy</font></font></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Basculer la navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
